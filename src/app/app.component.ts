5748e442056703628f622937f6435c7165ebb115
AidanLDev <dev@aidanlowson.com>
2023-12-07 22:21:11 +0000

    Set up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +14,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
     RouterOutlet,
     HeaderComponent,
     TasksComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Routes } from '@angular/router';
+import { TasksComponent } from './app/components/tasks/tasks.component';
+
+const appRoutes: Routes = [{ path: '', component: TasksComponent }];
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
